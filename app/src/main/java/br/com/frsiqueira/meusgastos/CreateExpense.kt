e65aa88601e7ec449f2f7352ae17389e92bca128
Felipe Siqueira <felipe.siqueira@b2wdigital.com>
2018-11-20 16:12:41 -0200

    Ajuste na funcionalidade de criar gasto

--- app/src/main/java/br/com/frsiqueira/meusgastos/CreateExpense.kt
+++ app/src/main/java/br/com/frsiqueira/meusgastos/CreateExpense.kt
@@ -3,11 +3,12 @@ package br.com.frsiqueira.meusgastos
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ArrayAdapter
+import br.com.frsiqueira.meusgastos.dto.Expense
 import kotlinx.android.synthetic.main.activity_create_expense.*
 
 class CreateExpense : AppCompatActivity() {
 
-    val paymentMethods = arrayOf("Debito", "Credito", "Dinheiro")
+    private val paymentMethods = arrayOf("Debito", "Credito", "Dinheiro")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +17,9 @@ class CreateExpense : AppCompatActivity() {
         val spinnerArrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, paymentMethods)
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spn_payment_method.adapter = spinnerArrayAdapter
+
+        btn_save_expense.setOnClickListener {
+            Expense(null, exp_decription.text.toString(), 0.0, spn_payment_method.selectedItem.toString())
+        }
     }
 }
---
+++ app/src/main/java/br/com/frsiqueira/meusgastos/dto/Expense.kt
@@ -0,0 +1,3 @@
+package br.com.frsiqueira.meusgastos.dto
+
+data class Expense(val id: Int?, val description: String, val amount: Double, val paymentMethod: String)
